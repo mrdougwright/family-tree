f32124503aeb253f944252e05be09e4331050f9e
Doug Wright <mrdougwright@gmail.com>
2014-06-29 00:46:19 -0700

    clean up code

--- form.js
+++ form.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
 					emptyFields = true;
 				}
 			});
-
 			if (emptyFields) {
 			  nextButton.attr('disabled', 'disabled');
 			} else {
@@ -24,17 +23,17 @@ $(document).ready(function(){
 		});
 	}
 
+  // parent form (index.html)
 	$('.parent_submit').click(function(){
-		if (validNames == false){
+		if (validNames == false) {
 			alert("A full name is required.");
-		}
-		else {
+		} else {
 			var newFamily = {};
 			newFamily.parents = [];
 			newFamily.children = [];
-			
 			var first_name = $("input[name='first_name']").val();
 			var last_name = $("input[name='last_name']").val();
+
 			newFamily.parents.push({
 				"first_name":first_name,
 	  		"last_name":last_name,
@@ -44,6 +43,7 @@ $(document).ready(function(){
 		}
 	});
 
+	// children form
 	$('#child_select').click(function(){
 		setTimeout(function(){
 			kidsText();
@@ -70,7 +70,7 @@ $(document).ready(function(){
 		var kidField = "<td><input name='first_name' type='text' placeholder='First Name'></td><td><input name='last_name' type='text' placeholder='Last Name'></td>";
 		for (var i = 0; i < 4; i++) {
 			if ($('#child_select')[0][i].selected == true)
-			  numFields = i;
+			  {numFields = i}
 			$(".kid_name"+i).empty();
 		}
 		for (var j = 0; j <= numFields; j++) {
@@ -79,27 +79,26 @@ $(document).ready(function(){
 	};
 
 	$('.child_submit').click(function(){
-		if (validNames == false){
+		if (validNames == false) {
 			alert("Please fill in required fields.");
 		} else {
-			var numFields = $("input[name='last_name']").length
+			var numFields = $("input[name='last_name']").length;
 			var family = retrieveData();
 
 			for (var x = 0; x < numFields; x++){
-				var first_name = $(".kid_name" + x + " input[name='first_name']").val()
-				var last_name = $(".kid_name" + x + " input[name='last_name']").val()
+				var first_name = $(".kid_name" + x + " input[name='first_name']").val();
+				var last_name = $(".kid_name" + x + " input[name='last_name']").val();
 	  		family.children.push({
 	  			"first_name":first_name,
 	  			"last_name":last_name,
 	  			"role":""
-	  		})
+	  		});
 	  	}
 	  	storeData(family);
 		}
 	});
 
-
-
+	// relationship form
 	function getChildren(){
 		var default_parent_role, default_child_role;
 		var children = retrieveData()['children'];
@@ -120,6 +119,38 @@ $(document).ready(function(){
 		fieldValidation();
 	}
 
+	function defaultRole(parent, child){
+		$('.prefill').each(function(){
+			$(".prefill input[name='parent_role']").val(parent);
+			$(".prefill input[name='child_role']").val(child);
+		});
+	}
+
+	$('.role_submit').click(function(){
+		if (validNames == false) {
+			alert("Please fill in required fields.");
+		} else {
+			var family = retrieveData();
+
+			for (var x = 0; x < family.parents.length; x++){
+				family.parents[x]['role'] = $(".kid_role" + x + " input[name='parent_role']").val();
+			}
+
+			for (var x = 0; x < family.children.length; x++){
+				var parent_role = $(".kid_role" + x + " input[name='parent_role']").val();
+				var child_role  = $(".kid_role" + x + " input[name='child_role']").val();
+				if (parent_role.toLowerCase() == "dad" || parent_role.toLowerCase() == "father") {
+					family.children[x].dad = family.parents[0].first_name;
+				} else {
+					family.children[x].mom = family.parents[0].first_name;
+				}
+				family.children[x]['role'] = child_role;
+	  	}
+	  	storeData(family);
+		}
+	});
+
+	// other parent form
 	function otherParents(){
 		var children = retrieveData()['children'];
 		var content = "<table><tbody><tr><td><span class='child_name'></span> has a</td></tr><tr><td><input name='parent_role' type='text' placeholder='Mom / Dad'></td></tr><tr><td colspan='2'>named</td></tr><tr><td><input name='first_name' type='text' placeholder='First Name'></td><td><input name='last_name' type='text' placeholder='Last Name'></td></tr></tbody></table>"
@@ -140,13 +171,6 @@ $(document).ready(function(){
 		fieldValidation();
 	}
 
-	function defaultRole(parent, child){
-		$('.prefill').each(function(){
-			$(".prefill input[name='parent_role']").val(parent);
-			$(".prefill input[name='child_role']").val(child);
-		});
-	}
-
 	function defaultParent(parent, first, last){
 		$('.prefill').each(function(){
 			$(".prefill input[name='parent_role']").val(parent);
@@ -155,31 +179,8 @@ $(document).ready(function(){
 		});
 	}
 
-	$('.role_submit').click(function(){
-		if (validNames == false){
-			alert("Please fill in required fields.");
-		} else {
-			var family = retrieveData();
-
-			for (var x = 0; x < family.parents.length; x++){
-				family.parents[x]['role'] = $(".kid_role" + x + " input[name='parent_role']").val();
-			}
-			for (var x = 0; x < family.children.length; x++){
-				var parent_role = $(".kid_role" + x + " input[name='parent_role']").val();
-				family.children[x]['role'] = $(".kid_role" + x + " input[name='child_role']").val();
-				
-				if (parent_role.toLowerCase() == "dad" || parent_role.toLowerCase() == "father") {
-					family.children[x].dad = family.parents[0].first_name;
-				} else {
-					family.children[x].mom = family.parents[0].first_name;
-				}
-	  	}
-	  	storeData(family);
-		}
-	});
-
 	$('.other_parent_submit').click(function(){
-		if (validNames == false){
+		if (validNames == false) {
 			alert("Please fill in required fields.");
 		} else {
 			var family = retrieveData();
@@ -212,18 +213,26 @@ $(document).ready(function(){
 		}
 	});
 
+  function search(first_name, myArray){
+		for (var i=0; i < myArray.length; i++) {
+			if (myArray[i].first_name == first_name) {
+				return myArray[i];
+			}
+		}
+	}
 
+
+  // final page
 	function getFamily() {
     var family = retrieveData();
     
     for (var y = 0;y < family.children.length; y++){
     	var person = family.children[y];
-    	var div = $('.family').append("<div class='member child" + y + "'></div>");
     	var father = getParent(person.dad);
     	var mother = getParent(person.mom);
     	var father_name = father.first_name +" "+ father.last_name;
     	var mother_name = mother.first_name +" "+ mother.last_name;
-
+    	$('.family').append("<div class='member child" + y + "'></div>");
     	$(".child"+y).html(person.first_name +" "+ person.last_name);
     	$(".child"+y).append("<div class='others'>"+father_name+" - "+father.role+"</div>");
     	$(".child"+y).append("<div class='others'>"+mother_name+" - "+mother.role+"</div>");
@@ -231,9 +240,8 @@ $(document).ready(function(){
 
     for (var i = 0; i < family.parents.length; i++) {
     	var person = family.parents[i];
-    	var div = $('.family').append("<div class='member parent" + i + "'></div>");
     	var kids = getKids(person.first_name);
-
+    	$('.family').append("<div class='member parent" + i + "'></div>");
     	$(".parent"+i).html(person.first_name +" "+ person.last_name);
 
 	    for (var x=0; x < kids.length; x++){
@@ -243,40 +251,37 @@ $(document).ready(function(){
     };
   }
 
-  function getKids(parent_name){
-  	var children = retrieveData()['children'];
-  	var legit_kids = [];
-  	for (var i=0; i< children.length; i++){
-  		if (children[i].dad == parent_name || children[i].mom == parent_name) {
-  			legit_kids.push(children[i]);
-  		}
-  	}
-  	return legit_kids;
-  }
   function getParent(first_name){
     var parents = retrieveData()['parents'];
   	for (var i=0; i < parents.length; i++){
   		if (parents[i].first_name == first_name) {
   			return parents[i];
   		}
+  		return false
   	}
   }
 
+  function getKids(parent_name){
+  	var children = retrieveData()['children'];
+  	var legit_kids = [];
+  	for (var i=0; i< children.length; i++){
+  		if (children[i].dad == parent_name || children[i].mom == parent_name)
+  			{ legit_kids.push(children[i]); }
+  	}
+  	return legit_kids;
+  }
 
+  $(".clear").click(function(){
+  	localStorage.clear();
+  });
+
+  // store and retrieve data from localStorage
 	function storeData(data){
 		localStorage.data = JSON.stringify(data);
 	}
 
 	function retrieveData(){
 		return JSON.parse(localStorage.data);
-	}
-
-	function search(first_name, myArray){
-		for (var i=0; i < myArray.length; i++) {
-			if (myArray[i].first_name == first_name) {
-				return myArray[i];
-			}
-		}
 	}
 
   fieldValidation();
